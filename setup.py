430a83b20d86b3c79b9fba7e3d498ab1340e6c38
Samukat <30996528+Samukat@users.noreply.github.com>
2020-05-14 14:29:52 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Pyfull-py",
+    version="0.0.1", #as of 14/05/2020
+    author="Panudio",
+    author_email="samuelkatz2245@gmail.com",
+    description="A small package for random functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Samukat/pyfull",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
